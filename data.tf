058bcd1b241109f5ff4439815dcb72051839efb9
hemanth <hemanthtadikonda29@gmail.com>
2023-12-05 07:42:20 +0530

    pc - "apps"

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
 data "aws_ssm_parameter" "master_username" {
-  name = "${var.env}.docdb.mater_username"
+  name = "${var.env}.docdb.master_username"
 }
 data "aws_ssm_parameter" "master_password" {
   name = "${var.env}.docdb.master_password"
